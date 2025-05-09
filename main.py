6a1f0b16506c2b55577ae045d7c4b142538b7abd
robert.homorogan <roberthomorogan.a@gmail.com>
2025-05-09 10:32:25 +0300

    upgraded show case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     user_action = input("Type add or show, edit, complete or exit: ")
-    user_action = user_action.strip() #removes all speces
+    user_action = user_action.strip() #removes all spaces
     match user_action:
         case "add":
             todo = input("Enter a todo: ") + "\n"
@@ -14,19 +14,24 @@ while True:
             file = open("todos.txt", "w")  #"w" writes on the files
             file.writelines(todos)
             file.close()
+
         case "show":
             file = open("todos.txt", "r")
             todos = file.readlines()
             file.close()
 
             for index, item in enumerate(todos): #enumerate function
-                print(f"{index + 1}-{item}")
+                item = item.strip("\n")   # remove the spaces in between
+                row = f"{index + 1}-{item}"
+                print(row)
+
         case "edit":
             print("Edit selected")
             number = int(input("Number of todos to edit: ")) #trabsforming str to int
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+
         case"complete":
             number = int(input("Number of todos to complete: "))
             todos.pop(number - 1)
